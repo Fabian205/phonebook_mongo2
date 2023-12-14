725884bcd6253797c18fde1e7b9c91433c63971e
Fabian205 <fabaguilar205@gmail.com>
2023-12-14 15:45:48 -0500

    commit package and persons.js

--- src/services/persons.jsx
+++ src/services/persons.jsx
@@ -1,27 +1,26 @@
-import axios from "axios";
-const baseUrl = "http://localhost:3000/api/persons";
-
+import axios from 'axios'
+const baseUrl = 'http://localhost:3000/api/persons'
 
 const getAll = () => {
-  const request = axios.get(baseUrl);
+  const request = axios.get(baseUrl)
   return request.then((response) => {
-    return response.data;
-  }); 
-};
+    return response.data
+  })
+}
 
 const create = (newObject) => {
-  const request = axios.post(baseUrl, newObject);
-  return request.then((response) => response.data);
-};
+  const request = axios.post(baseUrl, newObject)
+  return request.then((response) => response.data)
+}
 
 const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject);
-  return request.then((response) => response.data);
-};
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then((response) => response.data)
+}
 
 const remove = (id) => {
-  const request = axios.delete(`${baseUrl}/${id}`);
-  return request.then((response) => response.data);
-};
+  const request = axios.delete(`${baseUrl}/${id}`)
+  return request.then((response) => response.data)
+}
 
-export default { getAll, create, update, remove };
+export default { getAll, create, update, remove }
